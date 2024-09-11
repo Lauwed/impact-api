a24f25ae2b76730feb919f889afd6a42efbc174e
Lauwed <ldurieux97@gmail.com>
2024-09-11 18:42:39 +0200

    fix select

--- pwa/components/selectors/SourcesSelector.tsx
+++ pwa/components/selectors/SourcesSelector.tsx
@@ -5,9 +5,6 @@ import { Source } from "../../types";
 import Select from "../form/Select";
 import { fetcher } from "../utils/fetcher";
 
-import { AutoComplete } from 'primereact/autocomplete';
-        
-
 const SourcesSelector = ({
   value,
   onChange,
@@ -29,38 +26,22 @@ const SourcesSelector = ({
     }
   }, [data, isLoading]);
 
-  // const [value, setValue] = useState("");
-  const [items, setItems] = useState([]);
-
-  const search = (event) => {
-    setItems([...Array(10).keys()].map((item) => event.query + "-" + item));
-  };
-
   return (
-      <AutoComplete
-        value={value}
-        suggestions={items}
-        completeMethod={search}
-        onChange={onChange}
-      />
+    <Select
+      name="source"
+      id="source"
+      data={[
+        { value: -1, label: "" },
+        ...fields.map((field) => ({
+          value: field.id,
+          label: `${field.name} - ${TypeSources[field.typeSource.name]}`,
+        })),
+      ]}
+      required
+      value={value}
+      onChange={onChange}
+    />
   );
-
-  // return (
-  //   <Select
-  //     name="source"
-  //     id="source"
-  //     data={[
-  //       { value: -1, label: "" },
-  //       ...fields.map((field) => ({
-  //         value: field.id,
-  //         label: `${field.name} - ${TypeSources[field.typeSource.name]}`,
-  //       })),
-  //     ]}
-  //     required
-  //     value={value}
-  //     onChange={onChange}
-  //   />
-  // );
 };
 
 export default SourcesSelector;
