e1c8bb7a88b4e7725980262764f9372cd9dc9359
Paul Brissaud <pbrissaud.pro@gmail.com>
2024-09-13 11:53:19 +0200

    fix: fixture
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,9 @@
 namespace App\DataFixtures;
 
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
@@ -16,12 +19,14 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $user = $manager->getRepository(User::class)->findOneBy(['username' => 'admin']);
-        if (!$user) {
+        if ($user) {
             return;
         }
 
         $newUser = new User();
         $newUser->setUsername('admin');
+        $newUser->setEmail('admin@localhost');
+        $newUser->setRoles(['ROLE_ADMIN']);
 
         $password = $this->hasher->hashPassword($newUser, 'admin');
         $newUser->setPassword($password);
